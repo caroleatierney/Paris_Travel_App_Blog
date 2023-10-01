d3d2b689f2eac2158e0ad9ae0864136a666569f9
carole tierney <caroleatierney@gmail.com>
2023-09-30 22:05:01 -0400

    Minor changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,25 +12,16 @@ export default function Home() {
                     <p>fun background image</p>
                     <p>fun background color - image overladed - make in Canva</p>
 
-            <h2>Important</h2>
-                <h2>Saturday left to do</h2>
+                <h2>To Do Important</h2>
+                    <p>*** SUNDAY *** pass search form info into query data</p>
 
-                    <p>add data to mock api</p>
+                    <p>*** MONDAY *** add data to mock api</p>
+
                     <p>display data from mock api</p>
                     <p>and have delete options</p>
                     <p>Clear button restart</p>
-
-
-                <h2>Weekend stretch Goals</h2>
-                    <p>Set up search form</p>
-                    <p>pass in query data</p>
-
-                <h2>Monday</h2>
                     <p>Star rating for things we want to do most</p>
                     <p>form for entering photos and comments under photos</p>
-
-            <h2>Hard Stuff!</h2>
-                    <p>.env</p>
                     <p>blog</p>
 
 
