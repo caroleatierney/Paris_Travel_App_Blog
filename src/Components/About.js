18d692f7146b15cabbbd36c54d45cdf4a6743246
carole tierney <caroleatierney@gmail.com>
2023-10-07 12:02:25 -0400

    working on carousel

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,8 +5,8 @@ import './About.css';
 export default function About() {
     return (
         <div>
-            <Carousel className="p-3 mb-2 text-white">
-                <Carousel.Item className="p-3 mb-2 text-blue">
+            <Carousel className="p-3 mb-2 text-white" width={500} height={300}>
+                <Carousel.Item className="p-3 mb-2 text-yellow">
                     <img
                         className="d-block w-100"
                         src="https://imgur.com/NpmvFm6.png"
@@ -26,7 +26,7 @@ export default function About() {
                         alt="BasicTripImage"
                     />
 
-                    <Carousel.Caption className="p-3 mb-2 text-pink">
+                    <Carousel.Caption className="carousel-content">
                         <h1>ADRIENNE ITINERARY</h1>
                         <h2>SAINT-LEGER-SUR-DHEUNE to SAINT-JEAN-DE-LOSNE</h2>
                         <h3>Meeting Point: Hotel Raphael, Paris</h3>
@@ -41,7 +41,7 @@ export default function About() {
                         src="https://imgur.com/kZKuCsN.jpg"
                         alt="BasicTripImage"
                     /> 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-content">
                         <h1>Sunday, October 29, 2023</h1>
                         <p>The ADRIENNE's guide will meet you at the hotel and escort the transfer from Paris via motorcoach, to Saint Léger-sur-Dheune where the ADRIENNE is moored. Champagne Reception and Welcome Dinner on board.</p>
                     </Carousel.Caption>
@@ -53,7 +53,7 @@ export default function About() {
                         src="https://imgur.com/NpmvFm6.png"
                         alt="BasicTripImage"
                     /> 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-content">
                         <h1>Monday, October 30, 2023</h1>
                         <p>Morning cruise to Chagny. Early afternoon visit of the Château de Rully. Family-owned and occupied since its beginning in the 12th century, the current owner is your host for a remarkable stroll through his family's history. Din ner ashore at the three-star Michelin restaurant, Lameloise.</p>
                     </Carousel.Caption>
@@ -65,7 +65,7 @@ export default function About() {
                         src="https://imgur.com/NpmvFm6.png"
                         alt="BasicTripImage"
                     /> 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-content">
                         <h1>Tuesday, October 31, 2023</h1>
                         <p>Morning cruise along the picturesque Canal du Cen tre and Saône River to the city of Chalon-sur-Saône. afternoon, drive to the well-known wine town of Chassagne Montrachet for a private cellar tour and wine tasting at a prominent local vintner.</p>
                     </Carousel.Caption>
@@ -77,7 +77,7 @@ export default function About() {
                         src="https://imgur.com/NpmvFm6.png"
                         alt="BasicTripImage"
                     /> 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-content">
                         <h1>Wednesday, November 1, 2023</h1>
                         <p>Morning walking tour of Chalon-sur-Saône. Re turn to ADRIENNE for lunch on board. Beaune, the medieval wine capital of 15th century  Hospice and the historic to explore on your own. After lunch, drive toBurgundy. Visit the quarter, with free time to explore on your own.</p>
                     </Carousel.Caption>
@@ -89,7 +89,7 @@ export default function About() {
                         src="https://imgur.com/NpmvFm6.png"
                         alt="BasicTripImage"
                     /> 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-content">
                         <h1>Thursday, November 2, 2023</h1>
                         <p>Morning cruise to Seurre. Afternoon visit of the remarkable 12th-century museum château, the Clos de Vougeot, headquarters of the Chevaliers du Tastevin, whose special label is awarded annually to the finest Côte-d'Or wines.</p>
                     </Carousel.Caption>
@@ -101,7 +101,7 @@ export default function About() {
                         src="https://imgur.com/NpmvFm6.png"
                         alt="BasicTripImage"
                     /> 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-content">
                         <h1>Friday, November 3, 2023</h1>
                         <p>Morning cruise to the final mooring in Saint-Jean de-Losne. After lunch, enjoy a walking tour of Dijon, starting in front of the Ducal Palace, with time to browse in the fine specialty shops. Aperitifs and the Captain's Farewell Dinner on board.</p>
                     </Carousel.Caption>
@@ -113,7 +113,7 @@ export default function About() {
                         src="https://imgur.com/NpmvFm6.png"
                         alt="BasicTripImage"
                     /> 
-                    <Carousel.Caption>
+                    <Carousel.Caption className="carousel-content">
                         <h1>Wednesday, November 1, 2023</h1>
                         <p>After breakfast, depart the barge for the escorted transfer to Paris, arriving at the hotel at approxi mately 12:45 PM. Note: The Captain will schedule Hot Air Ballooning during the cruise upon request. Balloon flights are dependent on favorable wind conditions and a minimum of four passengers.</p>
                     </Carousel.Caption>
