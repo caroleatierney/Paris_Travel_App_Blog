c0b32d6e2064ef5733f59fd6a6b9555bcbaa6266
carole tierney <caroleatierney@gmail.com>
2023-10-09 22:03:02 -0400

    got photos, worked on itinerary

--- src/Components/Itinerary.js
+++ src/Components/Itinerary.js
@@ -13,84 +13,84 @@ export default function Itinerary() {
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://imgur.com/NpmvFm6.png"
-                    alt="BasicTripImage"
+                    src="https://i.imgur.com/ryqofuM.png"
+                    alt="Saint Léger-sur-Dheune"
                 />
                 <Carousel.Caption className="carousel-content">
-                    <h1>Sunday, October 29, 2023</h1>
-                    <p>The ADRIENNE's guide will meet you at the hotel and escort the transfer from Paris via motorcoach, to Saint Léger-sur-Dheune where the ADRIENNE is moored. Champagne Reception and Welcome Dinner on board.</p>
+                    <h1 className="beige">Sunday, October 29, 2023</h1>
+                    <h4 className="white">The ADRIENNE's guide will meet you at the hotel and escort the transfer from Paris via motorcoach, to Saint Léger-sur-Dheune where the ADRIENNE is moored. Champagne Reception and Welcome Dinner on board.</h4>
                 </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://imgur.com/NpmvFm6.png"
-                    alt="BasicTripImage"
+                    src="https://i.imgur.com/9RKQwqU.jpg"
+                    alt="Château de Rully"
                 />
                 <Carousel.Caption className="carousel-content">
-                    <h1>Monday, October 30, 2023</h1>
-                    <p>Morning cruise to Chagny. Early afternoon visit of the Château de Rully. Family-owned and occupied since its beginning in the 12th century, the current owner is your host for a remarkable stroll through his family's history. Din ner ashore at the three-star Michelin restaurant, Lameloise.</p>
+                    <h1 className="beige">Monday, October 30, 2023</h1>
+                    <h4 className="white">Morning cruise to Chagny. Early afternoon visit of the Château de Rully. Family-owned and occupied since its beginning in the 12th century, the current owner is your host for a remarkable stroll through his family's history. Dinner ashore at the three-star Michelin restaurant, Lameloise.</h4>
                 </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://i.imgur.com/dSAXcpm.png"
-                    alt="BasicTripImage"
+                    src="./Assetts/Chalon_Sur_Saone.jpeg"
+                    alt="Saône River"
                 />
                 <Carousel.Caption className="carousel-content">
-                    <h1>Tuesday, October 31, 2023</h1>
-                    <p>Morning cruise along the picturesque Canal du Cen tre and Saône River to the city of Chalon-sur-Saône. afternoon, drive to the well-known wine town of Chassagne Montrachet for a private cellar tour and wine tasting at a prominent local vintner.</p>
+                    <h1 className="beige">Tuesday, October 31, 2023</h1>
+                    <h4 className="white">Morning cruise along the picturesque Canal du Centre and Saône River to the city of Chalon-sur-Saône. afternoon, drive to the well-known wine town of Chassagne Montrachet for a private cellar tour and wine tasting at a prominent local vintner.</h4>
                 </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://imgur.com/NpmvFm6.png"
-                    alt="BasicTripImage"
+                    src="../Assetts/Chalon_Sur_Saone.jpeg"
+                    alt="Chalon-sur-Saône"
                 />
                 <Carousel.Caption className="carousel-content">
-                    <h1>Wednesday, November 1, 2023</h1>
-                    <p>Morning walking tour of Chalon-sur-Saône. Re turn to ADRIENNE for lunch on board. Beaune, the medieval wine capital of 15th century  Hospice and the historic to explore on your own. After lunch, drive toBurgundy. Visit the quarter, with free time to explore on your own.</p>
+                    <h1 className="beige">Wednesday, November 1, 2023</h1>
+                    <h4 className="white">Morning walking tour of Chalon-sur-Saône. Re turn to ADRIENNE for lunch on board. Beaune, the medieval wine capital of 15th century  Hospice and the historic to explore on your own. After lunch, drive toBurgundy. Visit the quarter, with free time to explore on your own.</h4>
                 </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://imgur.com/NpmvFm6.png"
-                    alt="BasicTripImage"
+                    src="https://i.imgur.com/3my3j6A.jpg"
+                    alt="Clos de Vougeot"
                 />
                 <Carousel.Caption className="carousel-content">
-                    <h1>Thursday, November 2, 2023</h1>
-                    <p>Morning cruise to Seurre. Afternoon visit of the remarkable 12th-century museum château, the Clos de Vougeot, headquarters of the Chevaliers du Tastevin, whose special label is awarded annually to the finest Côte-d'Or wines.</p>
+                    <h1 className="beige">Thursday, November 2, 2023</h1>
+                    <h4 className="white">Morning cruise to Seurre. Afternoon visit of the remarkable 12th-century museum château, the Clos de Vougeot, headquarters of the Chevaliers du Tastevin, whose special label is awarded annually to the finest Côte-d'Or wines.</h4>
                 </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://imgur.com/NpmvFm6.png"
-                    alt="BasicTripImage"
+                    src="xxx"
+                    alt="Saint-Jean de-Losne"
                 />
                 <Carousel.Caption className="carousel-content">
-                    <h1>Friday, November 3, 2023</h1>
-                    <p>Morning cruise to the final mooring in Saint-Jean de-Losne. After lunch, enjoy a walking tour of Dijon, starting in front of the Ducal Palace, with time to browse in the fine specialty shops. Aperitifs and the Captain's Farewell Dinner on board.</p>
+                    <h1 className="beige">Friday, November 3, 2023</h1>
+                    <h4 className="white">Morning cruise to the final mooring in Saint-Jean de-Losne. After lunch, enjoy a walking tour of Dijon, starting in front of the Ducal Palace, with time to browse in the fine specialty shops. Aperitifs and the Captain's Farewell Dinner on board.</h4>
                 </Carousel.Caption>
             </Carousel.Item>
 
             <Carousel.Item>
                 <img
                     className="d-block w-100"
-                    src="https://imgur.com/NpmvFm6.png"
-                    alt="BasicTripImage"
+                    src="./Assetts/Hot_Air_Balloon-Paris.jpeg"
+                    alt="Hot Air Balloons - Paris"
                 />
                 <Carousel.Caption className="carousel-content">
-                    <h1>Wednesday, November 1, 2023</h1>
-                    <p>After breakfast, depart the barge for the escorted transfer to Paris, arriving at the hotel at approxi mately 12:45 PM. Note: The Captain will schedule Hot Air Ballooning during the cruise upon request. Balloon flights are dependent on favorable wind conditions and a minimum of four passengers.</p>
+                    <h1 className="beige">Saturday, November 4, 2023</h1>
+                    <h4 className="white">After breakfast, depart the barge for the escorted transfer to Paris, arriving at the hotel at approxi mately 12:45 PM. Note: The Captain will schedule Hot Air Ballooning during the cruise upon request. Balloon flights are dependent on favorable wind conditions and a minimum of four passengers.</h4>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel >
